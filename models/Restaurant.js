e4e8f0c96a3a8d650c266b563dd4417cc3d5ca4d
DanJackson02 <dannyjjackson2002@gmail.com>
2023-02-15 16:37:03 +0000

    Starting code added to restaurant.js and db.js

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,17 @@
 const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
+//The `Restaurant` model should have name location and cuisine properties, all of which are strings
 // TODO - create a Restaurant model
+
+const sequelize = require("./db"); // Importing my connection from db.js
+const { DataTypes } = require("sequelize");
+
+const Restaurant = sequelize.define("Resturant", {
+  name: DataTypes.STRING,
+  location: DataTypes.STRING,
+  cuisine: DataTypes.STRING,
+//   rating: DataTypes.NUMBER,
+});
 
 module.exports = {Restaurant};
